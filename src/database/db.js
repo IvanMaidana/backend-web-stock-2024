c578a108f249465c45a3b519609460a3cc617c19
IvanMaidana <imadana30@gmail.com>
2024-06-17 12:11:10 -0300

    test deploy19

--- src/database/db.js
+++ src/database/db.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connetcDatabase = () => {
-
     try {
         mongoose.connect(process.env.MONGODB_URL, {});
         console.log("CONNECTED TO DATABASE SUCCESSFULLY");
     } catch (error) {
         console.error('COULD NOT CONNECT TO DATABASE:', error.message);
     }
-
 }
 
 module.exports = connetcDatabase;
